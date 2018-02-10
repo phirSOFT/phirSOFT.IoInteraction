edcf1f327c03bb87eb8c13139f4c6c9216fa330b
Philemon Eichin <phileicht@gmx.de>
2018-02-10 23:06:51 +0100

    Update OpenFileInteractionNotification

--- phirSOFT.IoInteraction/phirSOFT.IoInteraction/OpenFileInteractionNotification.cs
+++ phirSOFT.IoInteraction/phirSOFT.IoInteraction/OpenFileInteractionNotification.cs
@@ -1,14 +1,15 @@
-﻿using Prism.Interactivity.InteractionRequest;
+﻿using System.Collections.Generic;
+using Prism.Interactivity.InteractionRequest;
 
 namespace phirSOFT.IoInteraction
 {
     public class OpenFileInteractionNotification : Confirmation
     {
-        public string InitialName { get; internal set; }
-        public string Filter { get; internal set; }
-        public string[] FileNames { get; internal set; }
-        public int FilterIndex { get; internal set; }
-        public string InitialDirectory { get; internal set; }
-        public bool Multiselect { get; internal set; }
+        public string InitialName { get; set; }
+        public string Filter { get; set; }
+        public IEnumerable<string> FileNames { get; set; }
+        public int FilterIndex { get; set; }
+        public string InitialDirectory { get; set; }
+        public bool Multiselect { get; set; }
     }
 }
