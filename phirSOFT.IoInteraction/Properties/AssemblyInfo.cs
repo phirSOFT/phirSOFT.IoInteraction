2dc5a74e14a011e007bf083c4057d517ecf0f5af
Philemon Eichin <phileicht@gmx.de>
2018-02-10 23:29:22 +0100

    Update Assembly Information

--- phirSOFT.IoInteraction/Properties/AssemblyInfo.cs
+++ phirSOFT.IoInteraction/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("phirSOFT.IoInteraction")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides interaction notifications for file io.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("phirSOFT")]
 [assembly: AssemblyProduct("phirSOFT.IoInteraction")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
